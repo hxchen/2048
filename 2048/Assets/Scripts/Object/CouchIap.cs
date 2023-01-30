42452bb8debe96395b2abe31dcd3a989b4e4bee4
hxchen <helloworld.vip@gmail.com>
2023-01-30 23:18:37 +0800

    完善商店图片显示

--- 2048/Assets/Scripts/Object/CouchIap.cs
+++ 2048/Assets/Scripts/Object/CouchIap.cs
@@ -1,19 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static FullSerializer.fsAotVersionInfo;
 
 public class CouchIap : MonoBehaviour
 {
-    public Image couch;
+    public GameObject couch;
     public Text price;
     public Button button;
     public Text buttonText;
-
+    public Sprite[] couchesImages;
+    public List<string> string_index;
 
     /// <summary>
     /// 设置商品UI
     /// </summary>
     /// <param name="item"></param>
     public void SetItem(IapItem item) {
+        couch.GetComponent<Image>().sprite = couchesImages[string_index.IndexOf(item.itemId)];
         price.text = item.price.ToString();
         switch (item.status) {
             case IapItemStatusEnum.Using:
